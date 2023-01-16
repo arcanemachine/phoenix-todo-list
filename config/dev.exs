527d44779019713e6fb5abd25b945c7c420e4432
Nicholas Moen <arcanemachine@tutanota.com>
2023-01-16 15:59:18 -0700

    Setup dev HTTPS

--- config/dev.exs
+++ config/dev.exs
@@ -19,7 +19,13 @@ config :todo_list, TodoList.Repo,
 config :todo_list, TodoListWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
+  https: [
+    port: 4001,
+    cipher_suite: :strong,
+    keyfile: "priv/cert/key.pem",
+    certfile: "priv/cert/cert.pem"
+  ],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,

f4322797e7c749f936eda81c08abb49e26eb6853
Nicholas Moen <arcanemachine@tutanota.com>
2023-01-16 18:22:11 -0700

    Add DaisyUI

---
+++ assets/tailwind.config.cjs
@@ -0,0 +1,61 @@
+// See the Tailwind configuration guide for advanced usage
+// https://tailwindcss.com/docs/configuration
+
+const plugin = require("tailwindcss/plugin");
+
+module.exports = {
+  content: ["./js/**/*.js", "../lib/*_web.ex", "../lib/*_web/**/*.*ex"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#FD4F00",
+      },
+    },
+  },
+  daisyui: {
+    logs: false,
+    themes: [
+      {
+        default: {
+          primary: "#2563EB",
+          secondary: "#D1D5DB",
+          accent: "#8B5CF6",
+          neutral: "#312E81",
+          "base-100": "#F6F6FF",
+          info: "#7DD3FC",
+          success: "#25A762",
+          warning: "#FACC15",
+          error: "#DC2626",
+        },
+      },
+      "luxury",
+    ],
+  },
+  // mode: "jit",
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+    plugin(({ addVariant }) =>
+      addVariant("phx-no-feedback", [".phx-no-feedback&", ".phx-no-feedback &"])
+    ),
+    plugin(({ addVariant }) =>
+      addVariant("phx-click-loading", [
+        ".phx-click-loading&",
+        ".phx-click-loading &",
+      ])
+    ),
+    plugin(({ addVariant }) =>
+      addVariant("phx-submit-loading", [
+        ".phx-submit-loading&",
+        ".phx-submit-loading &",
+      ])
+    ),
+    plugin(({ addVariant }) =>
+      addVariant("phx-change-loading", [
+        ".phx-change-loading&",
+        ".phx-change-loading &",
+      ])
+    ),
+  ],
+};
--- config/config.exs
+++ config/config.exs
@@ -44,7 +44,7 @@ config :tailwind,
   version: "3.2.4",
   default: [
     args: ~w(
-      --config=tailwind.config.js
+      --config=tailwind.config.cjs
       --input=css/app.css
       --output=../priv/static/assets/app.css
     ),

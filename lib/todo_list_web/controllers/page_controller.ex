9ce7bb549b435bac003dee091a0ab6b4c11f6668
Nicholas Moen <arcanemachine@tutanota.com>
2023-01-17 20:07:07 -0700

    Use LiveView layout on homepage

--- lib/todo_list_web/controllers/page_controller.ex
+++ lib/todo_list_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule TodoListWeb.PageController do
   use TodoListWeb, :controller
 
   def home(conn, _params) do
-    render(conn, :home, layout: false, page_title: "Home")
+    render(conn, :home, page_title: "Home")
   end
 end

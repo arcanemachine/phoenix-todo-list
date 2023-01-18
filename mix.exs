b57c7e32d88d228454c8039c35e94fa041bba082
Nicholas Moen <arcanemachine@tutanota.com>
2023-01-17 20:04:48 -0700

    Add credo

--- mix.exs
+++ mix.exs
@@ -1,4 +1,7 @@
 defmodule TodoList.MixProject do
+  @moduledoc """
+  The TodoList project.
+  """
   use Mix.Project
 
   def project do
@@ -51,7 +54,8 @@ defmodule TodoList.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 

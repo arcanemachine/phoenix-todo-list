e5731fb2a39fc358024115a4b49f9f9785737408
Nicholas Moen <arcanemachine@tutanota.com>
2023-01-19 17:23:37 -0700

    Add button styles to user update template

--- lib/todo_list_web/live/user_settings_live.ex
+++ lib/todo_list_web/live/user_settings_live.ex
@@ -30,7 +30,7 @@ defmodule TodoListWeb.UserSettingsLive do
         required
       />
       <:actions>
-        <.button phx-disable-with="Changing...">Change Email</.button>
+        <.button class="btn btn-primary" phx-disable-with="Changing...">Change Email</.button>
       </:actions>
     </.simple_form>
 
@@ -64,7 +64,7 @@ defmodule TodoListWeb.UserSettingsLive do
         required
       />
       <:actions>
-        <.button phx-disable-with="Changing...">Change Password</.button>
+        <.button class="btn btn-primary" phx-disable-with="Changing...">Change Password</.button>
       </:actions>
     </.simple_form>
     """

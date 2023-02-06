904c9a289e3141eff73731ff06133129a4f4e52a
Nicholas Moen <arcanemachine@tutanota.com>
2023-02-05 19:28:31 -0700

    Configure server port via optional environment variable

--- config/dev.exs
+++ config/dev.exs
@@ -10,6 +10,8 @@ config :todo_list, TodoList.Repo,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
+port = String.to_integer(System.get_env("PORT") || "4000")
+
 # For development, we disable any cache and enable
 # debugging and code reloading.
 #
@@ -19,9 +21,9 @@ config :todo_list, TodoList.Repo,
 config :todo_list, TodoListWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {0, 0, 0, 0}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: port],
   https: [
-    port: 4001,
+    port: port + 1,
     cipher_suite: :strong,
     keyfile: "priv/cert/key.pem",
     certfile: "priv/cert/cert.pem"

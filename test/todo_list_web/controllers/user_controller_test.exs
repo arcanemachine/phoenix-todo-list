6e9d79f66dc30c754ef7b538bfd5e70926de9ddc
Nicholas Moen <arcanemachine@tutanota.com>
2023-02-06 01:28:16 -0700

    Undo changes caused by auth JSON generator

---
+++ test/todo_list_web/controllers/user_controller_test.exs
@@ -0,0 +1,82 @@
+defmodule TodoListWeb.UserControllerTest do
+  use TodoListWeb.ConnCase
+
+  import TodoList.AccountsFixtures
+
+  alias TodoList.Accounts.User
+
+  @create_attrs %{
+
+  }
+  @update_attrs %{
+
+  }
+  @invalid_attrs %{}
+
+  setup %{conn: conn} do
+    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+  end
+
+  describe "index" do
+    test "lists all users", %{conn: conn} do
+      conn = get(conn, ~p"/api/users")
+      assert json_response(conn, 200)["data"] == []
+    end
+  end
+
+  describe "create user" do
+    test "renders user when data is valid", %{conn: conn} do
+      conn = post(conn, ~p"/api/users", user: @create_attrs)
+      assert %{"id" => id} = json_response(conn, 201)["data"]
+
+      conn = get(conn, ~p"/api/users/#{id}")
+
+      assert %{
+               "id" => ^id
+             } = json_response(conn, 200)["data"]
+    end
+
+    test "renders errors when data is invalid", %{conn: conn} do
+      conn = post(conn, ~p"/api/users", user: @invalid_attrs)
+      assert json_response(conn, 422)["errors"] != %{}
+    end
+  end
+
+  describe "update user" do
+    setup [:register_user]
+
+    test "renders user when data is valid", %{conn: conn, user: %User{id: id} = user} do
+      conn = put(conn, ~p"/api/users/#{user}", user: @update_attrs)
+      assert %{"id" => ^id} = json_response(conn, 200)["data"]
+
+      conn = get(conn, ~p"/api/users/#{id}")
+
+      assert %{
+               "id" => ^id
+             } = json_response(conn, 200)["data"]
+    end
+
+    test "renders errors when data is invalid", %{conn: conn, user: user} do
+      conn = put(conn, ~p"/api/users/#{user}", user: @invalid_attrs)
+      assert json_response(conn, 422)["errors"] != %{}
+    end
+  end
+
+  describe "delete user" do
+    setup [:register_user]
+
+    test "deletes chosen user", %{conn: conn, user: user} do
+      conn = delete(conn, ~p"/api/users/#{user}")
+      assert response(conn, 204)
+
+      assert_error_sent 404, fn ->
+        get(conn, ~p"/api/users/#{user}")
+      end
+    end
+  end
+
+  defp register_user(_) do
+    user = user_fixture()
+    %{user: user}
+  end
+end

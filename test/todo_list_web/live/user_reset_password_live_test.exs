9a7bb847cef3759ed7eb9f3fc91e73493eba47f3
Nicholas Moen <arcanemachine@tutanota.com>
2023-02-08 00:15:49 -0700

    Change minimum password length from 12 characters to 8

--- test/todo_list_web/live/user_reset_password_live_test.exs
+++ test/todo_list_web/live/user_reset_password_live_test.exs
@@ -40,10 +40,10 @@ defmodule TodoListWeb.UserResetPasswordLiveTest do
         lv
         |> element("#reset_password_form")
         |> render_change(
-          user: %{"password" => "secret12", "confirmation_password" => "secret123456"}
+          user: %{"password" => "2short", "confirmation_password" => "secret123456"}
         )
 
-      assert result =~ "should be at least 12 character"
+      assert result =~ "should be at least 8 character"
       assert result =~ "does not match password"
     end
   end
@@ -75,14 +75,14 @@ defmodule TodoListWeb.UserResetPasswordLiveTest do
         lv
         |> form("#reset_password_form",
           user: %{
-            "password" => "too short",
+            "password" => "2short",
             "password_confirmation" => "does not match"
           }
         )
         |> render_submit()
 
       assert result =~ "Reset Password"
-      assert result =~ "should be at least 12 character(s)"
+      assert result =~ "should be at least 8 character(s)"
       assert result =~ "does not match password"
     end
   end

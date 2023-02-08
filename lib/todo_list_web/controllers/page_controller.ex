aaa905580282287206559471ebb48985b5202f23
Nicholas Moen <arcanemachine@tutanota.com>
2023-02-07 20:47:37 -0700

    Add page_title for homepage

--- lib/todo_list_web/controllers/page_controller.ex
+++ lib/todo_list_web/controllers/page_controller.ex
@@ -6,6 +6,6 @@ defmodule TodoListWeb.PageController do
     # so skip the default app layout.
     conn
     # |> put_flash(:info, "Hello world!")
-    |> render(:home, layout: false)
+    |> render(:home, layout: false, page_title: "Home")
   end
 end

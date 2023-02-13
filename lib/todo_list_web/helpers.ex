2270cf00e33595a2844066c472240d1cc5e7a26f
Nicholas Moen <arcanemachine@tutanota.com>
2023-02-13 02:39:10 -0700

    Merge helpers into single module

--- lib/todo_list_web/helpers.ex
+++ lib/todo_list_web/helpers.ex
@@ -1,7 +1,17 @@
+defmodule TodoListWeb.Helpers.Controller do
+  @moduledoc "Generic controller helper functions."
+
+  def http_response_403(conn) do
+    conn
+    |> Plug.Conn.put_status(403)
+    |> Phoenix.Controller.text("403 Forbidden")
+    |> Plug.Conn.halt()
+  end
+end
+
 defmodule TodoListWeb.Helpers.Ecto do
-  @moduledoc """
-  Helper functions for Ecto.
-  """
+  @moduledoc "Helper functions for Ecto."
+
   def changeset_errors_to_json(changeset) do
     Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
       Regex.replace(~r"%{(\w+)}", msg, fn _, key ->

ef01484a78dca550ab2a7be185e1a1d061d19653
Nicholas Moen <arcanemachine@tutanota.com>
2023-03-07 11:30:13 -0700

    Update Phoenix

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule TodoList.MixProject do
   defp deps do
     [
       {:bcrypt_elixir, "~> 3.0"},
-      {:phoenix, "~> 1.7.0-rc.2", override: true},
+      {:phoenix, "~> 1.7.1"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},

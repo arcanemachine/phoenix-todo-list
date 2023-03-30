91bff4eec958e42087fa4c94993d3681fca54e0b
Nicholas Moen <arcanemachine@tutanota.com>
2023-03-30 15:44:05 -0600

    Rewrite failing tests

--- test/todo_list_web/user_auth_test.exs
+++ test/todo_list_web/user_auth_test.exs
@@ -1,4 +1,6 @@
 defmodule TodoListWeb.UserAuthTest do
+  @moduledoc false
+
   use TodoListWeb.ConnCase, async: true
 
   alias Phoenix.LiveView
@@ -22,7 +24,7 @@ defmodule TodoListWeb.UserAuthTest do
       conn = UserAuth.login_user(conn, user)
       assert token = get_session(conn, :user_token)
       assert get_session(conn, :live_socket_id) == "users_sessions:#{Base.url_encode64(token)}"
-      assert redirected_to(conn) == ~p"/"
+      assert redirected_to(conn) == ~p"/todos/live"
       assert Accounts.get_user_by_session_token(token)
     end
 
@@ -216,7 +218,7 @@ defmodule TodoListWeb.UserAuthTest do
     test "redirects if user is authenticated", %{conn: conn, user: user} do
       conn = conn |> assign(:current_user, user) |> UserAuth.redirect_if_user_is_authenticated([])
       assert conn.halted
-      assert redirected_to(conn) == ~p"/"
+      assert redirected_to(conn) == ~p"/todos/live"
     end
 
     test "does not redirect if user is not authenticated", %{conn: conn} do

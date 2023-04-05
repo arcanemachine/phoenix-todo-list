f3e1c646ee9d87defe523bb7a82980cb8460f237
Nicholas Moen <arcanemachine@tutanota.com>
2023-04-05 13:54:23 -0600

    Create skeletons for todos_live tests

--- test/todo_list_web/live/todos_live_test.exs
+++ test/todo_list_web/live/todos_live_test.exs
@@ -28,4 +28,44 @@ defmodule TodoListWeb.TodosLiveTest do
       assert %{"error" => "You must log in to access this page."} = flash
     end
   end
+
+  describe "Todo form" do
+    test "creates a new todo" do
+    end
+
+    test "updates an existing todo" do
+    end
+  end
+
+  describe "Todo list" do
+    test "shows all todos" do
+    end
+
+    test "marks an incomplete todo as completed" do
+    end
+
+    test "marks a completed todo as incomplete" do
+    end
+
+    test "deletes a todo" do
+    end
+  end
+
+  describe "Presence" do
+    test "correctly shows when a single window is displaying the list" do
+    end
+
+    test "correctly shows when two windows are displaying the list" do
+    end
+  end
+
+  describe "PubSub" do
+    test "shows the correct message when the current window creates a todo" do
+    end
+
+    test "shows the correct message when a different window creates a todo" do
+    end
+
+    # repeat for update, toggle completion, delete...
+  end
 end

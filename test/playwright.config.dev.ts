db2dcca02c1f8a5828ea000f94f7605c2a8087c9
Nicholas Moen <arcanemachine@tutanota.com>
2023-04-07 12:15:23 -0600

    Move e2e tests up in the directory hierarchy

--- test/playwright.config.dev.ts
+++ test/playwright.config.dev.ts
@@ -11,7 +11,7 @@ import { devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
-  testDir: "./e2e/tests",
+  testDir: "./e2e",
 
   /* Maximum time one test can run for. */
   timeout: 6 * 1000,

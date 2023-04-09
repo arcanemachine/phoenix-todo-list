4914e0a9ba1a162d6971123f184d4378e69ce6e9
Nicholas Moen <arcanemachine@tutanota.com>
2023-04-08 22:07:56 -0600

    Show different actions for logged-out user

--- lib/todo_list_web/accounts/live/user_logout_live.ex
+++ lib/todo_list_web/accounts/live/user_logout_live.ex
@@ -7,25 +7,35 @@ defmodule TodoListWeb.UserLogoutLive do
 
   def render(assigns) do
     ~H"""
-    <%= if !assigns[:current_user] do %>
-      <!--script>
-        // if user is not authenticated, redirect them to homepage
-        window.location.replace('/');
-      </script-->
+    <%= if assigns[:current_user] do %>
+      <.header class="my-8 text-center">
+        Are you sure you want to log out?
+      </.header>
+
+      <.simple_form for={%{}} id="logout-form">
+        <:actions>
+          <.form_button_cancel />
+          <.link href={~p"/users/logout"} id="logout-form-button-submit" method="delete">
+            <.form_button tabindex="-1" class="btn-primary" content="Yes" />
+          </.link>
+        </:actions>
+      </.simple_form>
+    <% else %>
+      <.header class="my-8 text-center">
+        You are not currently logged in.
+      </.header>
+
+      <.simple_form for={%{}} id="logout-form">
+        <:actions>
+          <.link href={~p"/"}>
+            <.form_button tabindex="-1" class="btn-secondary" content="Home" />
+          </.link>
+          <.link href={~p"/users/login"}>
+            <.form_button tabindex="-1" class="btn-primary" content="Login" />
+          </.link>
+        </:actions>
+      </.simple_form>
     <% end %>
-
-    <.header class="my-8 text-center">
-      Are you sure you want to log out?
-    </.header>
-
-    <.simple_form for={%{}} id="logout-form">
-      <:actions>
-        <.form_button_cancel />
-        <.link href={~p"/users/logout"} id="logout-form-button-submit" method="delete">
-          <.form_button class="btn-primary" content="Yes" />
-        </.link>
-      </:actions>
-    </.simple_form>
     """
   end
 end

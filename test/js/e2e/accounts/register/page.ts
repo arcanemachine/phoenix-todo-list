9f474e02cd3e4894bef32257f1b21be4fde199ea
Nicholas Moen <arcanemachine@tutanota.com>
2023-04-12 17:26:12 -0600

    Add test cases to handle registration errors

--- test/js/e2e/accounts/register/page.ts
+++ test/js/e2e/accounts/register/page.ts
@@ -11,10 +11,16 @@ export class AccountsRegisterPage {
   readonly urlSuccess: string;
 
   // form elements
-  readonly formButtonSubmit: Locator;
   readonly inputEmail: Locator;
+  readonly errorEmail: Locator;
+
   readonly inputPassword: Locator;
+  readonly errorPassword: Locator;
+
   readonly inputPasswordConfirmation: Locator;
+  readonly errorPasswordConfirmation: Locator;
+
+  readonly formButtonSubmit: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -23,12 +29,28 @@ export class AccountsRegisterPage {
     this.url = urlBuild(urls.accounts.register);
     this.urlSuccess = urlBuild(urls.todos.todosLive);
 
-    // form elements
+    /* form elements */
+    // email
     this.inputEmail = page.locator("input[name='user[email]']");
+    this.errorEmail = page.locator(
+      "[phx-feedback-for='user[email]'] [data-component='error']"
+    );
+
+    // password
     this.inputPassword = page.locator("input[name='user[password]']");
+    this.errorPassword = page.locator(
+      "[phx-feedback-for='user[password]'] [data-component='error']"
+    );
+
+    // password confirmation
     this.inputPasswordConfirmation = page.locator(
       "input[name='user[password_confirmation]']"
     );
+    this.errorPasswordConfirmation = page.locator(
+      "[phx-feedback-for='user[password_confirmation]'] [data-component='error']"
+    );
+
+    // submit button
     this.formButtonSubmit = page
       .locator("#registration_form")
       .locator("button[type='submit']");
@@ -39,7 +61,27 @@ export class AccountsRegisterPage {
   }
 
   // actions
-  async register(email: string, password: string) {
+  async register(
+    email: string,
+    password: string,
+    options?: {
+      passwordConfirmation?: string;
+      submit?: boolean;
+    }
+  ) {
+    const defaultOptions = {
+      passwordConfirmation: undefined,
+      submit: true,
+    };
+
+    // merge options with default options
+    options = {
+      ...defaultOptions,
+      ...options,
+    };
+
+    const passwordConfirmation = options.passwordConfirmation || password;
+
     // fill out the form
     await this.inputEmail.click();
     await this.inputEmail.type(email);
@@ -48,9 +90,11 @@ export class AccountsRegisterPage {
     await this.inputPassword.type(password);
 
     await this.inputPasswordConfirmation.click();
-    await this.inputPasswordConfirmation.type(password);
+    await this.inputPasswordConfirmation.type(passwordConfirmation);
 
-    // submit the form
-    await this.formButtonSubmit.click();
+    if (options.submit) {
+      // submit the form
+      await this.formButtonSubmit.click();
+    }
   }
 }
--- test/js/e2e/accounts/register/test.spec.ts
+++ test/js/e2e/accounts/register/test.spec.ts
@@ -1,36 +1,78 @@
 import { test, expect } from "@playwright/test";
 
-import { passwordValid } from "test/support/constants";
+import {
+  emailInvalid,
+  errors,
+  passwordValid,
+  testUserEmail,
+} from "test/support/constants";
 import { emailGenerateRandom } from "e2e/support/helpers";
 import { AccountsRegisterPage } from "./page";
 
 test.describe("Account register page", () => {
   let accountsRegisterPage: AccountsRegisterPage;
-  let testUserEmail: string;
+  let randomEmail: string;
 
-  test.beforeAll(async () => {
-    // generate a new user on every test run
-    testUserEmail = emailGenerateRandom();
-  });
+  test.beforeAll(async () => {});
 
   test.beforeEach(async ({ page }) => {
+    randomEmail = emailGenerateRandom(); // generate a random email for each test
+
+    // navigate to test page
     accountsRegisterPage = new AccountsRegisterPage(page);
     await accountsRegisterPage.goto();
   });
 
   test("registers a new user", async ({ page }) => {
     // perform action
-    await accountsRegisterPage.register(testUserEmail, passwordValid);
+    await accountsRegisterPage.register(randomEmail, passwordValid);
 
     // redirects to expected page
-    expect(page).toHaveURL(accountsRegisterPage.urlSuccess);
+    await expect(page).toHaveURL(accountsRegisterPage.urlSuccess);
 
     // page contains expected success message
     await expect(page.getByText("Account created successfully")).toBeVisible();
   });
 
-  // test("shows error if email is invalid", async ({ baseURL, page }) => {});
-  // test("shows error if email is taken", async ({ baseURL, page }) => {});
-  // test("shows error if password is too short", async ({ baseURL, page }) => {});
-  // test("shows error if passwords do not match", async ({ baseURL, page }) => {});
+  test("shows expected error if email is invalid", async () => {
+    // perform action
+    await accountsRegisterPage.register(emailInvalid, passwordValid, {
+      submit: false,
+    });
+
+    // form contains expected error
+    await expect(accountsRegisterPage.errorEmail).toBeVisible();
+    await expect(accountsRegisterPage.errorEmail).toHaveText(
+      errors.email.isInvalid
+    );
+  });
+
+  test("shows expected error if email is taken", async () => {
+    const takenEmail = testUserEmail;
+
+    // perform action
+    await accountsRegisterPage.register(takenEmail, passwordValid);
+
+    // form contains expected error
+    await expect(accountsRegisterPage.errorEmail).toBeVisible();
+    await expect(accountsRegisterPage.errorEmail).toHaveText(
+      errors.email.isTaken
+    );
+  });
+
+  test("shows expected error if passwords do not match", async () => {
+    const nonMatchingPassword = passwordValid + "a";
+
+    // perform action
+    await accountsRegisterPage.register(randomEmail, passwordValid, {
+      passwordConfirmation: nonMatchingPassword,
+      submit: false,
+    });
+
+    // form contains expected error
+    await expect(accountsRegisterPage.errorPasswordConfirmation).toBeVisible();
+    await expect(accountsRegisterPage.errorPasswordConfirmation).toHaveText(
+      errors.passwordConfirmation.doesNotMatch
+    );
+  });
 });

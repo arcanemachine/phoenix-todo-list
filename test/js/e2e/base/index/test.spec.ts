706d95d920be34aff6494b2adb14dc47b21020ad
Nicholas Moen <arcanemachine@tutanota.com>
2023-04-13 22:37:53 -0600

    Use more confident test description

--- test/js/e2e/base/index/test.spec.ts
+++ test/js/e2e/base/index/test.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from "@playwright/test";
 import { BaseIndexPage } from "./page";
 
 test.describe("Base index page", () => {
-  test("should contain expected title", async ({ page }) => {
+  test("contains expected title", async ({ page }) => {
     // navigate to test page
     const baseIndex = new BaseIndexPage(page);
     await baseIndex.goto();

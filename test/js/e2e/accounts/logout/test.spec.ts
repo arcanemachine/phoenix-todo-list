0fbe3d3f7b3714e97d8fb2633a59aaa1374302b2
Nicholas Moen <arcanemachine@tutanota.com>
2023-04-16 17:06:19 -0600

    Do not use authenticated worker session to for E2E logout tests

--- test/js/e2e/accounts/logout/test.spec.ts
+++ test/js/e2e/accounts/logout/test.spec.ts
@@ -1,39 +1,50 @@
-import unauthenticatedTest from "@playwright/test";
+/**
+ * These tests use the generic Playwright `test()` method to avoid issues
+ * caused by logging out of the session used by authenticated workers.
+ */
+import test from "@playwright/test";
 import { expect } from "@playwright/test";
+import { AccountsLoginPage } from "test/e2e/accounts/login/page";
+import { passwordValid, testUserEmail } from "test/support/constants";
 
-import { authenticatedTest } from "test/e2e/support/fixtures";
 import { AccountsLogoutPage } from "./page";
 
-unauthenticatedTest.describe("[Unauthenticated] Account logout page", () => {
+test.describe("[Unauthenticated] Account logout page", () => {
   let testPage: AccountsLogoutPage;
 
-  unauthenticatedTest.beforeEach(async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     // navigate to test page
     testPage = new AccountsLogoutPage(page);
     await testPage.goto();
   });
 
-  unauthenticatedTest("shows expected form buttons", async () => {
+  test("shows expected form buttons", async () => {
     await expect(testPage.formButtonHome).toBeVisible();
     await expect(testPage.formButtonLogin).toBeVisible();
   });
 });
 
-authenticatedTest.describe("[Authenticated] Account logout page", () => {
+test.describe("[Authenticated] Account logout page", () => {
   let testPage: AccountsLogoutPage;
+  let accountsLoginPage: AccountsLoginPage;
 
-  authenticatedTest.beforeEach(async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
+    // login as generic test user
+    accountsLoginPage = new AccountsLoginPage(page);
+    await accountsLoginPage.goto();
+    await accountsLoginPage.login(testUserEmail, passwordValid);
+
     // navigate to test page
     testPage = new AccountsLogoutPage(page);
     await testPage.goto();
   });
 
-  authenticatedTest("shows expected form buttons", async () => {
+  test("shows expected form buttons", async () => {
     await expect(testPage.formButtonCancel).toBeVisible();
     await expect(testPage.formButtonSubmit).toBeVisible();
   });
 
-  authenticatedTest("logs out an authenticated user", async ({ page }) => {
+  test("logs out an authenticated user", async ({ page }) => {
     // perform action
     await testPage.logout();
 

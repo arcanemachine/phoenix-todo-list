471abbf038717f7b0c26cbe804cd5165198e0b0a
Nicholas Moen <arcanemachine@tutanota.com>
2023-04-18 22:20:39 -0600

    Upgrade 'credo' and add 'dialyxir'

--- mix.exs
+++ mix.exs
@@ -53,7 +53,8 @@ defmodule TodoList.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false},
       {:gen_smtp, "~> 1.2"},
       {:open_api_spex, "~> 3.16"},
       {:ymlr, "~> 3.0"}

e48dafda4ee4b09ed922e7591e12e9efbe28d797
Nicholas Moen <arcanemachine@tutanota.com>
2023-04-20 15:17:15 -0600

    Bug fixes and performance improvements

--- config/test.exs
+++ config/test.exs
@@ -17,13 +17,13 @@ config :todo_list, TodoList.Repo,
   pool_size: 10
 
 host = System.get_env("PHX_HOST") || "localhost"
-port = String.to_integer(System.get_env("PORT") || "4000")
+port = String.to_integer(System.get_env("PORT") || "4000") + 2
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :todo_list, TodoListWeb.Endpoint,
-  http: [ip: {0, 0, 0, 0}, port: port + 2],
-  check_origin: ["http://#{host}:#{port + 2}"],
+  http: [ip: {0, 0, 0, 0}, port: port],
+  check_origin: ["http://#{host}:#{port}"],
   secret_key_base: "Ygp6edO5FTklaUFXjkSnuF7y8alcXyb/cU/J1BZH34cvOANEO/U+37Q7hpGR+3ff",
   server: true
 

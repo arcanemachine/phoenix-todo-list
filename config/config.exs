3945925fd345120fbe468e6e47eac10132301b8e
Nicholas Moen <arcanemachine@tutanota.com>
2023-04-29 11:46:16 -0600

    chore: upgrade tailwind (3.2.4 -> 3.3.2)

--- config/config.exs
+++ config/config.exs
@@ -48,7 +48,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "3.2.4",
+  version: "3.3.2",
   default: [
     args: ~w(
       --config=tailwind.config.cjs

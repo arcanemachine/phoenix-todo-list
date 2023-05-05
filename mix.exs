eab0c21d1d38af411afa82467042756ef6ab0394
Nicholas Moen <arcanemachine@tutanota.com>
2023-05-05 00:55:33 -0600

    chore: bump Elixir tailwind from 0.1.8 to 0.2

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule TodoList.MixProject do
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.7.2"},
       {:esbuild, "~> 0.5", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.1.8", runtime: Mix.env() == :dev},
+      {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
       {:finch, "~> 0.13"},
       {:telemetry_metrics, "~> 0.6"},

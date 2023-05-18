40e68e928e519655248619d954c6e32032370b98
Nicholas Moen <arcanemachine@tutanota.com>
2023-05-18 02:21:05 -0600

    style: simplify comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -58,17 +58,18 @@ let liveSocket = new LiveSocket("/live", Socket, {
   hooks: Hooks,
 });
 
-Alpine.store("liveSocket", liveSocket); // add alpine store for livesocket
+// add alpine store for livesocket
+Alpine.store("liveSocket", liveSocket);
 
-// Show progress bar on live navigation and form submits
+// show progress bar on live navigation and form submits
 topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
 window.addEventListener("phx:page-loading-start", () =>
   topbar.delayedShow(200)
 );
 window.addEventListener("phx:page-loading-stop", () => topbar.hide());
 
-// connect if there are any LiveViews on the page
+// connect to any liveviews on the page
 liveSocket.connect();
 
-// expose liveSocket on window for web console debug logs and latency simulation:
+// expose livesocket on window for debugging
 window.liveSocket = liveSocket;

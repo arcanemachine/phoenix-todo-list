1c7fd89212365a792de9a571a6793357b1b45116
Nicholas Moen <arcanemachine@tutanota.com>
2023-05-18 03:52:50 -0600

    refactor: move Alpine.JS init logic back into app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,12 +24,36 @@ import { LiveSocket } from "phoenix_live_view";
 import topbar from "../vendor/topbar";
 import Hooks from "./hooks";
 
-// setup Alpine.JS
-import initAlpine from "./init/alpine";
-const Alpine = initAlpine();
+// setup alpine
+import Alpine from "alpinejs";
+import collapse from "@alpinejs/collapse";
+import focus from "@alpinejs/focus";
+import {
+  data as alpineData,
+  directives as alpineDirectives,
+  stores as alpineStores,
+} from "js/alpine";
+
+// plugins
+Alpine.plugin(collapse);
+Alpine.plugin(focus);
+
+for (const data of alpineData) {
+  Alpine.data(data.name, data.data);
+}
+
+for (const directive of alpineDirectives) {
+  Alpine.directive(directive.name, directive.directive);
+}
+
+for (const store of alpineStores) {
+  Alpine.store(store.name, store.store);
+}
+
+Alpine.start();
 window.Alpine = Alpine;
 
-// setup LiveView
+// setup liveview
 let csrfToken = document
   .querySelector("meta[name='csrf-token']")
   .getAttribute("content");

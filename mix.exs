114392f29421aadde94d60197e529b61e21b75c6
Nicholas Moen <arcanemachine@tutanota.com>
2023-05-25 19:23:21 -0600

    chore: bump project version: 0.1.5 -> 0.1.6

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TodoList.MixProject do
   def project do
     [
       app: :todo_list,
-      version: "0.1.5",
+      version: "0.1.6",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,

3d46dd777ffa5d22b77049929981691c52466134
Nicholas Moen <arcanemachine@tutanota.com>
2023-07-10 18:04:31 -0600

    chore: use ~c sigil when generating random characters with 'Enum.random/1'

--- lib/todo_list/helpers.ex
+++ lib/todo_list/helpers.ex
@@ -2,6 +2,6 @@ defmodule TodoList.Helpers do
   @moduledoc false
 
   def generate_random_string(length \\ 32) do
-    for _ <- 1..length, into: "", do: <<Enum.random('0123456789abcdef')>>
+    for _ <- 1..length, into: "", do: <<Enum.random(~c"0123456789abcdef")>>
   end
 end

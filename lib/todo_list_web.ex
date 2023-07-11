f8bc53b49a3cca1dcd89deb06facc763fa9bd9a9
Nicholas Moen <arcanemachine@tutanota.com>
2023-07-10 18:02:56 -0600

    chore: add 'sitemap.xml'

--- lib/todo_list_web.ex
+++ lib/todo_list_web.ex
@@ -17,7 +17,7 @@ defmodule TodoListWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt static)
+  def static_paths, do: ~w(assets fonts images static favicon.ico robots.txt sitemap.xml)
 
   def router do
     quote do
